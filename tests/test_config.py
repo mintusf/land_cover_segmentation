d9ea56f2156a8cff326f0153abc4a3a57a6931f1
fmintus <fmintus@ridge-i.com>
2021-08-08 10:04:40 +0000

    Add config

---
+++ tests/test_config.py
@@ -0,0 +1,13 @@
+import os
+
+from config.default import get_cfg_defaults
+
+
+def test_config():
+    cfg = get_cfg_defaults()
+    cfg.merge_from_file(os.path.join("config", "tests.yml"))
+    cfg.freeze()
+
+    assert "MODEL" in cfg and "DATASET" in cfg
+    assert cfg.MODEL.TYPE == "DeepLab"
+    assert cfg.DATASET.ROOT == "/data"
