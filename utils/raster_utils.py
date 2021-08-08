ad0269dcb6439d6b8e45ba9ee6f44d30634fe0ab
fmintus <fmintus@ridge-i.com>
2021-08-08 09:15:22 +0000

    Add docstring

--- utils/raster_utils.py
+++ utils/raster_utils.py
@@ -1,15 +1,17 @@
 from typing import List
 
+from rasterio.io import DatasetReader
 
-def get_coord_from_raster(raster) -> List[float]:
-    """[summary]
+def get_coord_from_raster(raster: DatasetReader) -> List[float]:
+    """ Gets coordintes from raster
 
     Args:
-        raster_filename (str): [description]
+        raster (DatasetReader): Rasterio dataset reader
 
     Returns:
-        List[float]: [description]
+        List[float]: Coordinates x0, y0, x1, y1
     """
+    print(type(raster))
     (x0, y0) = raster.xy(0, 0)
     (x1, y1) = raster.xy(raster.height, raster.width)
 
