ffd4fca839dd1ee2b537ef05f0598d231a307216
fmintus <fmintus@ridge-i.com>
2021-08-08 10:30:10 +0000

    Remove printing

--- tests/test_config.py
+++ tests/test_config.py
@@ -7,7 +7,6 @@ def test_config():
     cfg = get_cfg_defaults()
     cfg.merge_from_file(os.path.join("config", "tests.yml"))
     cfg.freeze()
-    print(type(cfg))
 
     assert "MODEL" in cfg and "DATASET" in cfg
     assert cfg.MODEL.TYPE == "DeepLab"
