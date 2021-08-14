5d75c554833658861623f81a4f84e78be76138d1
fmintus <fmintus@ridge-i.com>
2021-08-14 02:45:41 +0000

    Formatting fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ import pytest
 
 from config.default import get_cfg_from_file
 
+
 @pytest.fixture(scope="session")
 def test_config():
     cfg = get_cfg_from_file(os.path.join("config", "tests.yml"))
