4291c10050475c922a4f95a77abaf6b9d8d2e2de
fmintus <fmintus@ridge-i.com>
2021-08-26 10:34:11 +0000

    Update container setup

--- env/setup_docker.sh
+++ env/setup_docker.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 # You need to change followings:
 REPOSITORY_PATH="/home/fmintus/land_cover_tracking"
-DOCKERFILE_PATH="./docker"
-IMAGE_NAME="fmintus/rtc_02_model_classification:latest"
-CONTAINER_NAME="fmintus_rtc02_model_classification"
-GPU='"device=0,1,2,3"'
-MEMORY="12G"
+DOCKERFILE_PATH="./env"
+IMAGE_NAME="fmintus/study:latest"
+CONTAINER_NAME="fmintus_study"
+GPU='"device=8,9,10,11"'
+MEMORY="24G"
+DATA_PATH="/raid/fmintus/sentinel_land_cover/seg_data"
 
 cd $REPOSITORY_PATH
 echo "Creating docker image"
-docker build --network host -t $IMAGE_NAME DOCKERFILE_PATH --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g)
+docker build --network host -t $IMAGE_NAME $DOCKERFILE_PATH --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g)
 echo "Finished !"
 
 echo "Creating docker container"
 docker run --gpus $GPU \
            --net host \
            -v $REPOSITORY_PATH:/workspace \
-           -v /raid/RTC_02/recieved_data:/received_data \
-           -v /mnt/USB_HDD:/received_data2 \
+           -v $DATA_PATH:/data \
            --shm-size=$MEMORY \
            --name $CONTAINER_NAME \
            -itd $IMAGE_NAME
