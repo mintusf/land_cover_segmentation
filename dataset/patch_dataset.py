ec7301d679f91c5b1086b845867b1d761422ab0f
mintuf <mintusf@gmail.com>
2021-08-26 23:35:47 +0000

    Fix dataset_list_path

--- dataset/patch_dataset.py
+++ dataset/patch_dataset.py
@@ -28,15 +28,15 @@ class PatchDataset(Dataset):
         self.target_sensor_name = cfg.DATASET.MASK.SENSOR
 
         if mode == "train":
-            self.dataset_list_path = get_lines_from_txt(cfg.DATASET.LIST_TRAIN)
+            self.dataset_list_path = cfg.DATASET.LIST_TRAIN
         elif mode == "val":
-            self.dataset_list_path = get_lines_from_txt(cfg.DATASET.LIST_VAL)
+            self.dataset_list_path = cfg.DATASET.LIST_VAL
         elif mode == "test":
-            self.dataset_list_path = get_lines_from_txt(cfg.DATASET.LIST_TEST)
+            self.dataset_list_path = cfg.DATASET.LIST_TEST
         else:
             raise ValueError(f"Unknown mode: {mode}")
 
-        self.dataset_list = self.dataset_list_path
+        self.dataset_list = get_lines_from_txt(self.dataset_list_path)
 
         self.transforms = transforms
         self.device = cfg.TRAIN.DEVICE
