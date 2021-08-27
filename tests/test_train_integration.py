c9c2bf2452931b342f8fb232882af3dbe976f60f
mintuf <mintusf@gmail.com>
2021-08-27 04:02:09 +0000

    Fix test_train_integration

--- tests/test_train_integration.py
+++ tests/test_train_integration.py
@@ -5,7 +5,7 @@ from train import run_training
 
 
 def test_train_integration(test_config):
-    os.mkdir(test_config.TRAIN.WEIGHTS_FOLDER)
+    os.makedirs(test_config.TRAIN.WEIGHTS_FOLDER, exist_ok=True)
     run_training("config/tests.yml")
 
     rmtree(test_config.TRAIN.WEIGHTS_FOLDER)
