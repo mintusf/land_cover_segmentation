6b63213ea1410d21404abd83fc448af01060c89a
mintuf <mintusf@gmail.com>
2021-09-04 11:26:28 +0000

    Implement inference into raster

--- infer.py
+++ infer.py
@@ -17,9 +17,9 @@ from models.models_utils import (
     rename_ordered_dict_to_parallel,
 )
 from train_utils import load_checkpoint
-from utils.raster_utils import convert_np_for_vis
+from utils.raster_utils import convert_np_for_vis, np_to_raster
 from utils.visualization_utils import create_alphablend
-from utils.utilities import split_sample_name, get_gpu_count
+from utils.utilities import split_sample_name, get_gpu_count, get_raster_filepath
 
 
 def parser():
@@ -51,36 +51,37 @@ def parser():
         "--destination",
         help="Path for saving results",
         type=str,
-        default="/data/seg_data/masks",
+        default="/data/seg_data/inference",
     )
 
-    # TODO Add raster generation support
     parser.add_argument(
         "--outputs",
         nargs="+",
-        default=["alphablend"],
+        default=["alphablend", "raster", "alphablended_raster"],
         help="What kind of outputs to generate",
     )
 
     return parser.parse_args()
 
 
-def get_alphablend_path(name: str, alphablend_destination: str) -> str:
-    """Returns a path for alphablended sample.
+def get_save_path(
+    name: str, destination: str, suffix: str, extention: str = "png"
+) -> str:
+    """Returns a path for an output sample.
         Creates directory if doesn't exist
 
     Args:
         name (str): Sample name
-        alphablend_destination (str): Alphablend root directory
-
+        destination (str): Root directory
+        suffix (str): Suffix for file
     Returns:
-        str: Path to alphablended sample
+        str: Save path
     """
     roi_folder, area, _ = split_sample_name(name)
-    alphablend_folder = os.path.join(alphablend_destination, roi_folder, area)
+    alphablend_folder = os.path.join(destination, roi_folder, area)
     if not os.path.isdir(alphablend_folder):
         os.makedirs(alphablend_folder)
-    alphablend_path = os.path.join(alphablend_folder, f"{name}_alphablend.png")
+    alphablend_path = os.path.join(alphablend_folder, f"{name}_{suffix}.{extention}")
 
     return alphablend_path
 
@@ -122,9 +123,69 @@ def generate_save_alphablend(
         alphablend_destination (str): Root path to save alphablend
     """
     input_img, mask = prepare_tensors_for_vis(input_img, mask)
-    alphablended = create_alphablend(input_img, mask, mask_config)
-    alphablend_path = get_alphablend_path(name, alphablend_destination)
+    alpha = mask_config["alpha"]
+    colors_dict = mask_config["colors"]
+    alphablended = create_alphablend(input_img, mask, alpha, colors_dict)
+    alphablend_path = get_save_path(name, alphablend_destination, "alphablend")
     cv2.imwrite(alphablend_path, alphablended)
+
+
+def generate_save_raster(mask, name, mask_config, ref_raster, raster_destination):
+    mask = mask.cpu().numpy()
+    dummy_image = np.ones((mask.shape[0], mask.shape[1], 3))
+    alpha = 1.0
+    colors_dict = mask_config["colors"]
+    alphablended = create_alphablend(dummy_image, mask, alpha, colors_dict)
+    colored_mask = alphablended.transpose(2, 0, 1)
+    raster_path = get_save_path(name, raster_destination, "raster", extention="tif")
+    np_to_raster(colored_mask, ref_raster, raster_path)
+
+
+def generate_save_alphablended_raster(
+    mask, input_img, name, mask_config, ref_raster, raster_destination
+):
+    input_img, mask = prepare_tensors_for_vis(input_img, mask)
+    alpha = mask_config["alpha"]
+    colors_dict = mask_config["colors"]
+    alphablended = create_alphablend(input_img, mask, alpha, colors_dict)
+    alphablended = alphablended.transpose(2, 0, 1)
+    raster_path = get_save_path(
+        name, raster_destination, "alphablended_raster", extention="tif"
+    )
+    np_to_raster(alphablended, ref_raster, raster_path)
+
+
+def generate_outputs(
+    output_types, destination, input_img, mask, name, mask_config, dataloader
+):
+    if "alphablend" in output_types:
+        alphablend_destination = os.path.join(destination, "alphablend")
+        generate_save_alphablend(
+            input_img, mask, name, mask_config, alphablend_destination
+        )
+
+    ref_raster_path = get_raster_filepath(
+        dataloader.dataset.dataset_root,
+        name,
+        dataloader.dataset.input_sensor_name,
+    )
+
+    if "raster" in output_types:
+        raster_destination = os.path.join(destination, "raster")
+        generate_save_raster(
+            mask, name, mask_config, ref_raster_path, raster_destination
+        )
+    if "alphablended_raster" in output_types:
+        raster_destination = os.path.join(destination, "alphablended_raster")
+
+        generate_save_alphablended_raster(
+            mask,
+            input_img,
+            name,
+            mask_config,
+            ref_raster_path,
+            raster_destination,
+        )
 
 
 def infer(
@@ -159,10 +220,15 @@ def infer(
 
             for input_img, mask, name in zip(inputs, masks, names):
 
-                if "alphablend" in output_types:
-                    generate_save_alphablend(
-                        input_img, mask, name, mask_config, destination
-                    )
+                generate_outputs(
+                    output_types,
+                    destination,
+                    input_img,
+                    mask,
+                    name,
+                    mask_config,
+                    dataloader,
+                )
 
 
 def run_infer(
