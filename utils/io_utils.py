a33fdea85e15236c272df8bf63010349365291c5
mintuf <mintusf@gmail.com>
2021-09-14 11:56:17 +0000

    Add removed areas

--- utils/io_utils.py
+++ utils/io_utils.py
@@ -1,11 +1,15 @@
 import json
-from typing import List
 import random
+from typing import List
 import yaml
 from yaml import CLoader as Loader
 
 
-def get_lines_from_txt(txt_path: str, shuffle: bool = False) -> List[str]:
+def get_lines_from_txt(
+    txt_path: str,
+    shuffle: bool = False,
+    removed_subgrids=["ROIs1158_spring_24", "ROIs1158_spring_26"],
+) -> List[str]:
     """Generates list of filenames from a txt file"""
     try:
         with open(txt_path, "r") as f:
@@ -13,6 +17,10 @@ def get_lines_from_txt(txt_path: str, shuffle: bool = False) -> List[str]:
         all_filenames = [filename.strip("\n") for filename in all_filenames]
         if shuffle:
             random.shuffle(all_filenames)
+        for subgrid in removed_subgrids:
+            all_filenames = [
+                filename for filename in all_filenames if subgrid not in filename
+            ]
     except FileNotFoundError:
         all_filenames = []
     return all_filenames
