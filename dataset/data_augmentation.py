441838c99e1bf6b95f6c44162d6538f56af376f1
mintuf <mintusf@gmail.com>
2021-10-03 07:39:54 +0000

    Fix augmentation transforms

--- dataset/data_augmentation.py
+++ dataset/data_augmentation.py
@@ -1,27 +1,38 @@
 from albumentations import Compose
-from albumentations.augmentations.transforms import RandomBrightnessContrast
+from albumentations.augmentations.transforms import HorizontalFlip, VerticalFlip
 from albumentations.augmentations.crops.transforms import RandomResizedCrop
 
 from yacs.config import CfgNode
 
 
-def get_augmentation_transforms(cfg: CfgNode):
+def get_augmentation_transforms(cfg: CfgNode) -> Compose:
+    """Returns the augmentation transforms for the dataset.
+
+    Args:
+        cfg (CfgNode): The configuration node.
+
+    Returns:
+        Compose: The augmentation transforms.
+    """
     transforms = []
     # Add data augmentation
-    if cfg.DATA_AUG.RANDOM_RESIZE_CROP.USE:
+    if cfg.TRAIN.DATA_AUG.RANDOM_RESIZE_CROP.USE:
         transform = RandomResizedCrop(
             height=cfg.DATASET.SHAPE[0],
             width=cfg.DATASET.SHAPE[1],
-            scale=cfg.DATA_AUG.RANDOM_RESIZE_CROP.SCALE,
-            ratio=cfg.DATA_AUG.RANDOM_RESIZE_CROP.RATIO,
-            p=cfg.DATA_AUG.RANDOM_RESIZE_CROP.PROBABILITY,
+            scale=cfg.TRAIN.DATA_AUG.RANDOM_RESIZE_CROP.SCALE,
+            ratio=cfg.TRAIN.DATA_AUG.RANDOM_RESIZE_CROP.RATIO,
+            p=cfg.TRAIN.DATA_AUG.RANDOM_RESIZE_CROP.PROBABILITY,
         )
         transforms.append(transform)
-    if cfg.DATA_AUG.RANDOM_BRIGHTNESS_CONTRAST.USE:
-        transform = RandomBrightnessContrast(
-            brightness_limit=cfg.DATA_AUG.RANDOM_BRIGHTNESS_CONTRAST.BRIGHTNESS_CHANGE,
-            contrast_limit=cfg.DATA_AUG.RANDOM_BRIGHTNESS_CONTRAST.CONTRAST_CHANGE,
-            p=cfg.DATA_AUG.RANDOM_BRIGHTNESS_CONTRAST.PROBABILITY,
+    if cfg.TRAIN.DATA_AUG.HORIZONTAL_FLIP.USE:
+        transform = HorizontalFlip(
+            p=cfg.TRAIN.DATA_AUG.HORIZONTAL_FLIP.PROBABILITY,
+        )
+        transforms.append(transform)
+    if cfg.TRAIN.DATA_AUG.VERTICAL_FLIP.USE:
+        transform = VerticalFlip(
+            p=cfg.TRAIN.DATA_AUG.VERTICAL_FLIP.PROBABILITY,
         )
         transforms.append(transform)
 
